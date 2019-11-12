c74720483210c3ed994d6d23ce651782bf37bae0
PFayoux <paul.fayoux@gmail.com>
2019-11-12 16:09:08 +0100

    Practice - Parse arguments

--- my-hello-world-cli/server.js
+++ my-hello-world-cli/server.js
@@ -1,2 +1,28 @@
 #!/usr/bin/env node
-console.log('Hello CLI')
+
+const pkg = require('./package')
+
+const options = process.argv.slice(2)
+
+const VERSION_MESSAGE =`
+    my-hello-world-cli ${pkg.version}
+`
+const HELP_MESSAGE = `
+    my-hello-world-cli
+
+    ${pkg.description}
+    ${pkg.version}
+
+    Usage:
+    --help    Help documentation
+    --version Installed package version
+`
+switch (true) {
+    case options.includes('--version'):
+        console.log(VERSION_MESSAGE)
+        break;
+
+    default:
+        console.log(HELP_MESSAGE)
+        break;
+}
